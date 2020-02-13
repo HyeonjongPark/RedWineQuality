d1fcc607996279bbff7b75525b82c556a135cf0a
HyeonjongPark <guswhd9844@gmail.com>
2020-02-13 16:02:45 +0900

    prob 추가

--- 03src/07random-forest.R
+++ 03src/07random-forest.R
@@ -7,12 +7,26 @@ rf_model <- randomForest(quality~alcohol+volatile_acidity+citric_acid+
                            density+pH+sulphates,train)
 
 rf_result <- predict(rf_model, newdata = valid[,!colnames(valid) %in% c("quality")])
+rf_result_proba <- predict(rf_model, newdata = valid[,!colnames(valid) %in% c("quality")], type = "prob")
 
-
+rf_result_proba
 
 confusionMatrix(rf_result, valid$quality)
 
 varImp(rf_model) %>% kable()
 varImpPlot(rf_model)
 
-rm(rf_model, rf_result)
+
+
+
+## tuning
+
+fitControl = trainControl(method = "repeatedcv", number = 10, repeats = 5)
+rf_fit = train(quality~alcohol+volatile_acidity+citric_acid+
+                 density+pH+sulphates , data=train, method = "rf", trControl = fitControl, verbose = F )
+
+rf_fit
+
+rf_fit_result = predict(rf_fit, newdata = valid[,!colnames(valid) %in% c("quality")])
+confusionMatrix(rf_fit_result, valid$quality)
+
