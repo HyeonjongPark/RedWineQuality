5412c50ac491e980e2626ed183a138109a984910
HyeonjongPark <guswhd9844@gmail.com>
2020-02-13 16:02:53 +0900

    prob 추가

--- 03src/10svm.R
+++ 03src/10svm.R
@@ -5,10 +5,22 @@ set.seed(1)
 
 svm_model <- svm(quality~alcohol+volatile_acidity+citric_acid+
                    
-                   density+pH+sulphates,train)
+                   density+pH+sulphates, train, gamma = 1, cost = 2)
+
+?svm
 
 svm_result <- predict(svm_model, newdata = valid[,!colnames(valid) %in% c("quality")])
 
 
 
 confusionMatrix(svm_result, valid$quality)
+
+
+# 최적 하이퍼 파라메터 값 찾기
+
+obj <- tune.svm(quality~., data = train, gamma = 2^(-2:2), cost = 2^(1:5))
+
+obj
+summary(obj)
+plot(obj)
+
